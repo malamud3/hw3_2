dee703bc5e22d211ef7b58324c56fd29940baf95
malamud3 <malamud95@gmail.com>
2022-01-01 10:41:37 +0200

    3
    3

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const expressLayouts= require('express-ejs-layouts')
 const mongoose = require('mongoose')
 
+//GUI
+app.set('view engine','ejs')
+app.set('views',__dirname + '/views')
+app.set('layout','layots/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
 
-mongoose.connect(process.env.DATABASE_URL)
+//DATABASE
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser : true, useUnifiedTopology:true })
 const db= mongoose.connection
 db.on('error',(error)=> console.error(error))
 db.once('open',()=> console.log('Connected to Database'))
@@ -13,4 +21,4 @@ app.use(express.json())
 
 const sitesRouter= require('./routes/sites')
 app.use('/sites',sitesRouter)
-app.listen(3000, ()=> console.log('server Started'))
+app.listen(process.env.PORT || 3000, ()=> console.log('server Started'))
